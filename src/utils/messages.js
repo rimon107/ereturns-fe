2211c4c52be3f3890133ccc69bad33936973e102
Md Nahidul Islam <nahidulislam107@gmail.com>
2022-03-15 15:52:43 +0600

    refactoring.

---
+++ src/utils/messages.js
@@ -0,0 +1,17 @@
+// Error Messages
+export const ERROR = {
+  SERVER_ERROR:
+    "Internal Server Error. Please contact Bangladesh Bank Admin for further query.",
+  OTHER_ERROR:
+    "Somthings went wrong. Please contact Bangladesh Bank Admin for further query.",
+  SELECT_FI: "Please select Bank/NBFI first.",
+  DATA_LOAD_ERROR: "Data is not loaded correctly. Please try again.",
+  HO_USER_LIMIT: "Head Office user limit exceeded.",
+  BRANCH_USER_LIMIT: "Branch user limit exceeded.",
+  WRONG_SBS_CODE:
+    "Wrong SBS Code. Please Contact With the Authenticated Person.",
+  USERNAME: "Please provide username.",
+  PASSWORD: "Please provide password.",
+};
+
+// export const SUCCESS = {};
